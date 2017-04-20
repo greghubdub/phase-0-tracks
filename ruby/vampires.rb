8563ac0da9dd4fd6f0dc999bc7cf04dc0bc39b05
greghubdub <gregnagy10@gmail.com>
2017-04-20 01:03:58 -0500

    Create vampire interview questions release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+puts "What is your name?"
+
+my_name = gets.chomp
+
+puts "How old are you?"
+
+my_age = gets.chomp
+
+puts "What year were you born?"
+
+my_birth_year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
+
+want_garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+
+want_insurance = gets.chomp
+
