da399312524eb84f219e6e3460f77e5471b034eb
greghubdub <gregnagy10@gmail.com>
2017-04-21 14:12:17 -0500

    Match conditions to interview results Release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,20 +1,54 @@
+probably_not_a_vampire = false
+
+probably_a_vampire = false
+
+almost_certainly_a_vampire = false
+
+definitely_a_vampire = false
+
 puts "What is your name?"
 
 my_name = gets.chomp
 
 puts "How old are you?"
 
-my_age = gets.chomp
+my_age = gets.chomp.to_i
 
 puts "What year were you born?"
 
-my_birth_year = gets.chomp
+my_birth_year = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
 
 want_garlic = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? y/n"
 
 want_insurance = gets.chomp
 
+if ((my_age == 2017 - my_birth_year) || (my_age == 2017 - (my_birth_year + 1))) && (want_insurance == 'y' || want_garlic == 'y')
+	probably_not_a_vampire = true
+end
+
+if ((my_age != 2017 - my_birth_year) && (my_age != 2017 - (my_birth_year + 1))) && (want_insurance == 'n' || want_garlic == 'n')
+	probably_a_vampire = true
+end
+
+if ((my_age != 2017 - my_birth_year) && (my_age != 2017 - (my_birth_year + 1))) && want_garlic == 'n' && want_insurance == 'n'
+	almost_certainly_a_vampire = true
+end
+
+if my_name == 'Drake Cula' || my_name == 'Tu Fang'
+	probably_not_a_vampire = false
+	definitely_a_vampire = true
+end
+
+interview_result = case
+	when probably_not_a_vampire then "Probably not a vampire"
+	when probably_a_vampire then "Probably a vampire"
+	when almost_certainly_a_vampire then "Almost certainly a vampire"
+	when definitely_a_vampire then "Definitely a vampire"
+	else "Results inconclusive"
+end
+
+puts interview_result
