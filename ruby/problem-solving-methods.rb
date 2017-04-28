8c4948a8518ea537746aa9c10823f681747fe5fd
greghubdub <gregnagy10@gmail.com>
2017-04-28 13:04:15 -0500

    First draft solved fibonacci

--- ruby/problem-solving-methods.rb
+++ ruby/problem-solving-methods.rb
@@ -1,5 +1,5 @@
 # Release 0
-
+=begin
 def search_array(arr, num)
 	count = 0
 	arr.each do |x| 
@@ -13,4 +13,29 @@ end
 p search_array([5, 4, 3, 2], 3)
 
 # Release 1
+=end
+#=begin
+	
+
+def fib(bit)
+	fib_array = [0]
+	h = 0
+ 	i = 1
+ 	j = 0
+ 	if bit >= 2
+ 		fib_array = [0, 1]
+		until fib_array.count == bit
+			j = h + i
+			h = i
+			i = j
+			fib_array << j
+		end
+	end
+p fib_array
+end
+
+fib(45)
+
+#=end
+
 
