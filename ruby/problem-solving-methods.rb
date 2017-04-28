78b5e0b352a3831ee489460eb1d47ef445aa40bd
greghubdub <gregnagy10@gmail.com>
2017-04-28 11:40:22 -0500

    Solved Release 0 search method

--- ruby/problem-solving-methods.rb
+++ ruby/problem-solving-methods.rb
@@ -3,13 +3,14 @@
 def search_array(arr, num)
 	count = 0
 	arr.each do |x| 
-		if num == arr[x]
-			p x
+		if x == num
+			p count
 		end
+		count += 1
 	end
 end
 
-p search_array([5, 4, 3, 2], 4)
+p search_array([5, 4, 3, 2], 3)
 
 # Release 1
 
