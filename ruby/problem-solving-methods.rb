d7548a2e07dd083fc21a1081d66cdcf4339d0bc9
greghubdub <gregnagy10@gmail.com>
2017-04-28 11:07:56 -0500

    First save code from Jenna's driving during our session

---
+++ ruby/problem-solving-methods.rb
@@ -0,0 +1,25 @@
+def search_array(arr, num)
+	puts arr
+	puts num
+	arr.each {|x| index_array << x }
+	count = count+1
+	end
+end
+
+
+
+search_array([72, 95, 26, 1], 95)
+
+def search_array(array, x)
+    new_array = []
+    array.each {|item| new_array << item }
+end
+
+def search_array(arr, num)
+	a = arr
+	count = 0
+  when a.count "num" >= 1
+	puts 
+	count = count+1
+	end
+end
