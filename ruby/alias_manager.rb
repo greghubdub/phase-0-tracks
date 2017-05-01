0821e46cbf8fc14b186309dfc68c414ee9a3ad41
greghubdub <gregnagy10@gmail.com>
2017-05-01 09:09:38 -0500

    Start pseudocode for alias algorithm

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+# split name into array with names as elements
+
+# swap names
+
+# back to string
+
+# split swapped name into array with letters as elements
+
+# downcase
+
+# create a method that gives next vowel
+
+# create a method that gives next consonant
+
+# handle edge cases
+
+# back to string
