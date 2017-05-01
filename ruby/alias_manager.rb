fc5d8944a1e23a5e22240afc32be9fa43e12fcd5
greghubdub <gregnagy10@gmail.com>
2017-05-01 13:09:47 -0500

    Final alias manager

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,9 +2,45 @@
 
 # get name
 
+letters_alias = (('b'..'z').to_a + ['a']).join('')
+
+original_array = []
+
+alias_array = []
+
 puts 'Please enter name'
 
-original_name = gets.chomp
+original_name = gets.chomp.downcase
+
+while original_name != "quit" do
+
+	original_array << original_name
+	alias_name = original_name.tr('a-z', letters_alias)
+	puts alias_name
+	alias_array << alias_name
+	original_name = gets.chomp.downcase
+
+end
+
+# p original_array
+
+# p alias_array
+
+# p original_array[0]
+
+x = 0
+
+until x == original_array.count
+	puts "#{alias_array[x]} is actually #{original_array[x]}"
+	x += 1
+end
+
+puts "Mission debriefed"
+
+
+=begin
+
+My original work below. I had to find a simpler aliasing method and move on instead of using it
 
 # split name into array with names as elements
 
@@ -34,11 +70,16 @@ p letters_array
 
 # create a method that gives next vowel
 
+
 vowel_ref = ['a', 'e', 'i', 'o', 'u']
 
 letters_array.select do |x|
-	vowel_ref.include?(x)
+	if letters_array.include?(vowel_ref(x))
+		letters_array.map { |y| y == vowel_ref(x) ? }
+	end
 end
+
+p letters_array
 
 def vowel
 
@@ -49,3 +90,7 @@ end
 # handle edge cases
 
 # back to string
+
+
+
+=end
