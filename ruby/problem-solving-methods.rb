1cdbae934c48fc83dbe6097c8963304cc071d42d
greghubdub <gregnagy10@gmail.com>
2017-05-01 09:55:43 -0500

    Implement Release 2 sorting method

--- ruby/problem-solving-methods.rb
+++ ruby/problem-solving-methods.rb
@@ -35,7 +35,34 @@ fib(15)
 
 # Release 2
 
+# Bubble sort
 
+# There is a visualization I found on wikipedia that really helps,
+# showing the algorithm taking each pair at a time
+
+# I'm somewhat overwhelmed, also excited to learn this!
+
+# Pseudocode
+
+# Have the code consider 0 and 1 index, compare value
+
+# If larger value is in lower position, swap them
+
+# Move on to 1 and 2 index and repeat. Continue until array is finished
+
+def gregsort(arr)
+	x = 0
+	y = 1
+	while y < arr.count do
+		if arr[x] > arr[y]
+			arr[x], arr[y] = arr[y], arr[x]
+		end
+		x += 1
+		y += 1
+	end
+end
+
+gregsort([7, 4, 3, 8, 10])
 
 
 
