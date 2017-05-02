b0cebb9f04ca27cec29c3bea1f39f487fe941862
greghubdub <gregnagy10@gmail.com>
2017-05-02 14:41:18 -0500

    Create Kitty method and complete puppy methods 6.2 pairing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -27,6 +27,10 @@ class Puppy
 	end
   end
 
+  def initialize
+  	puts "Initializing new Puppy instance..."
+  end
+
 end
 
 doggo = Puppy.new
@@ -43,3 +47,40 @@ doggo.shake("fido")
 
 doggo.shake("doggo")
 
+class Kitty
+
+  def initialize
+  	puts "Initializing new Kitty instance..."
+  end
+
+  def scratch(frn)
+  	puts "*scratch scratch scratch on the #{frn}"
+  end
+
+  def meow(words)
+  	puts "*human*: #{words}"
+  	puts "*kitty*: meooow!!"
+  end
+
+
+end
+
+boots = Kitty.new
+
+boots.scratch("ottoman")
+
+boots.meow("Are you hungry?")
+
+kitty_array = []
+
+50.times do | x |
+	x = Kitty.new
+	kitty_array << x 
+end
+
+kitty_array.each do |y|
+	y.scratch("chair")
+	y.meow("Playtime!")
+end
+
+p kitty_array
