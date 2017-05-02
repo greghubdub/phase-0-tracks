439b0b81bc889ce99bf91f8440cc0db0e385def5
greghubdub <gregnagy10@gmail.com>
2017-05-02 13:41:15 -0500

    Blueprint puppy class using comments for Release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+	
+SPECIES ------------------------
+Pupster doggus
+
+CHARACTERISTICS ----------------
+Softness: superlative
+Friendly: yes
+Little paws count: 4
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Romp
+Cuddle
+Scamper
+	
+=end
