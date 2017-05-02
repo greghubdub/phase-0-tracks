d904c7e0e91899570cb0262ccdb805703dd6fe76
greghubdub <gregnagy10@gmail.com>
2017-05-02 14:20:09 -0500

    Create methods for Puppy class release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,41 @@ class Puppy
     toy
   end
 
+  def rollover
+  	puts "*rolls over*"
+  end
+
+  def dogyears(hyrs)
+  	hyrs * 7
+  end
+
+  def shake(name)
+  	if name == "doggo"
+  		puts "*shakes paw*"
+  	else
+  		puts "Arf?"
+  	end
+  end
+
+  def speak(int)
+	int.times do 
+	puts "Woof!"
+	end
+  end
+
 end
+
+doggo = Puppy.new
+
+doggo.fetch("ball")
+
+doggo.speak(4)
+
+doggo.rollover
+
+puts doggo.dogyears(5)
+
+doggo.shake("fido")
+
+doggo.shake("doggo")
 
