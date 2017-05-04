7bb79a709ae6a45672ffeb951b138e84a98b2b49
greghubdub <gregnagy10@gmail.com>
2017-05-04 11:25:38 -0500

    Create mixin Shout module and get people yellin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,6 @@
+=begin
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
@@ -12,3 +13,30 @@ end
 puts Shout.yell_angrily("Aw, no! Not on the GOLF CART")
 
 puts Shout.yelling_happily("Zumbaa")
+
+=end
+
+module Shout
+	def yell_angrily(words)
+		puts "Ahhh NO NO NO!!! #{words.upcase}!!!!!!!"
+	end
+
+	def yelling_happily(words)
+		puts "YEEESSSS!!! Finally! #{words.upcase}!!!!!!!!!!"
+	end
+end
+
+class Dad
+	include Shout
+end
+
+class Teacher
+	include Shout
+end
+
+dad = Dad.new
+dad.yelling_happily("The bees are extinct")
+
+teacher = Teacher.new
+teacher.yell_angrily("i before e except, ah, whatever")
+
