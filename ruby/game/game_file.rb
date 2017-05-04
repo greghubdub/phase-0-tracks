100582156749af34e49bdb45031661c9aec754af
greghubdub <gregnagy10@gmail.com>
2017-05-04 18:01:23 -0500

    WordGame up and running

--- ruby/game/game_file.rb
+++ ruby/game/game_file.rb
@@ -15,20 +15,40 @@
 	
 class WordGame
 
-#	attr_accessor :word
-#	attr_accessor :feedback
-#	attr_reader :guess_count
-#	attr_reader :is_over
+	attr_accessor :word
+	attr_accessor :feedback
+	attr_accessor :word_array
+	attr_writer :guess
+	attr_reader :guess_count
+	attr_reader :is_over
+	attr_reader :guess_array
 
 	def initialize(word)
 		@word = word
+		@word_array = word.split('')
+		@guess_array = []
 		@feedback = "_" * word.length
 		@guess_count = 0
 		@is_over = false
 	end
 
 	def check_guess(guess)
-		
+		@guess = guess
+		if !@guess_array.include? guess
+			@guess_count += 1
+		end
+		@guess_array << guess
+		@word_array.each do |i|
+			if i == guess
+				@feedback[@word_array.index(guess)] = guess
+			end
+		end
+		if @guess_count == word.length * 2 || @feedback == @word
+			@is_over = true
+		else
+			false
+		end
+		@feedback
 	end
 
 end
@@ -37,4 +57,20 @@ end
 
 puts "Come one, come all, and seek thy fourtune in the Word Game!!!"
 
-game = WordGame.new
+game = WordGame.new("demon")
+
+while !game.is_over
+	puts "Please guess a letter in the mystery word"
+	guess = gets.chomp
+	if !game.check_guess(guess)
+		puts "Nope! Try again."
+	end
+	p game.feedback
+	p game.guess_count
+end
+
+if game.feedback == game.word
+	puts "You've won! The word is #{game.word.upcase} and the world is YOURS!!!"
+else
+	puts "May your soul find no solace for all eternity - YOU LOSE"
+end
--- ruby/game/game_file_spec.rb
+++ ruby/game/game_file_spec.rb
@@ -1,12 +1,11 @@
 require_relative 'game_file'
 
 describe WordGame do
-	let(:game) {WordGame.new}
+	let(:game) {WordGame.new("house")}
 
-	it "checks the guess against the word"
-		expect(game.check_guess("c")).to eq true
+	it "checks the guess against the word" do
+		expect(game.check_guess("o")).to eq "_o___"
 	end
-
 
 
 end
