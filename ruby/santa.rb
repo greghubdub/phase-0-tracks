e03edbb474f459dc327f47713f929cf4e9e24e7f
greghubdub <gregnagy10@gmail.com>
2017-05-03 20:36:51 -0500

    Build many, many Santas Release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+	attr_reader :ethnicity, :reindeer_ranking, :age
+	attr_accessor :gender
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -14,26 +16,6 @@ class Santa
 		@ethnicity = ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
-	end
-
-	def gender
-		@gender
-	end
-
-	def gender=(new_gender)
-		@gender = new_gender
-	end
-
-	def ethnicity
-		@ethnicity
-	end
-
-	def age
-		@age
-	end
-
-	def reindeer_ranking
-		@reindeer_ranking
 	end
 
 	def celebrate_birthday(new_age)
@@ -62,15 +44,15 @@ santas << Santa.new("female", "Latina")
 
 santas << Santa.new("male", "unspecified")
 
-example_genders = ["agender", "bigender", "gender fluid", "female", "male"]
+example_genders = ["agender", "bigender", "gender fluid", "female", "male", "starchild"]
 
-example_ethnicities = ["black", "Latino", "white", "Samoan", "grey"]
+example_ethnicities = ["black", "Latino", "white", "Samoan", "grey", "Australian aboriginal"]
 
 example_genders.length.times do |x|
 	santas << Santa.new(example_genders[x], example_ethnicities[x])
 end
 
-example_cookies = ["chocolate chip cookie", "monkey bar", "biscuit", "digestive", "brownie"]
+example_cookies = ["chocolate chip cookie", "monkey bar", "biscuit", "digestive", "brownie", "toffee cookie"]
 
 example_cookies.length.times do |y|
 	santas[y].eat_milk_and_cookies(example_cookies[y])
@@ -93,3 +75,9 @@ saint_nick.gender = "bro"
 p saint_nick.gender
 
 p saint_nick.ethnicity
+
+400.times do |i|
+	i = Santa.new(example_genders.sample, example_ethnicities.sample)
+	i.celebrate_birthday(1 + rand(140))
+	p i
+end
