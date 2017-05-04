2ea122a795d53ec1617708cbf62eae4196cb07b5
greghubdub <gregnagy10@gmail.com>
2017-05-03 20:14:44 -0500

    Add getters n setters Release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -12,8 +12,36 @@ class Santa
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
+	end
+
+	def gender
+		@gender
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def age
+		@age
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	def celebrate_birthday(new_age)
+		@age = new_age
+	end
+
+	def get_mad_at(reindeer)
+		reindeer_ranking.insert(-1, reindeer_ranking.delete_at(reindeer_ranking.index(reindeer)))
 	end
 
 end
@@ -47,3 +75,21 @@ example_cookies = ["chocolate chip cookie", "monkey bar", "biscuit", "digestive"
 example_cookies.length.times do |y|
 	santas[y].eat_milk_and_cookies(example_cookies[y])
 end
+
+p saint_nick.age
+
+saint_nick.celebrate_birthday(5)
+
+p saint_nick.age
+
+saint_nick.get_mad_at("Rudolph")
+
+p saint_nick.reindeer_ranking
+
+p saint_nick.gender
+
+saint_nick.gender = "bro"
+
+p saint_nick.gender
+
+p saint_nick.ethnicity
