6e60ef7da16147e7077f9be7d9bfb61fb3ebe355
greghubdub <gregnagy10@gmail.com>
2017-05-10 13:39:57 -0500

    Create object and put horse names and colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,12 +1,19 @@
+var horseList = {};
+
 var colorsArr = ["turquiose", "aqua", "cerulean", "azure"];
 var namesArr = ["Musty", "Misty", "Stallone", "Bob"];
 
 console.log(colorsArr);
 console.log(namesArr);
 
-colorsArr.push("Indigo");
+colorsArr.push("indigo");
 namesArr.push("Valerie");
 
 console.log(colorsArr);
 console.log(namesArr);
 
+for (var i = 0; i < namesArr.length; i++) {
+	horseList[namesArr[i]] = colorsArr[i];
+}
+
+console.log(horseList);
