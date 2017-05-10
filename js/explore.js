0008ecde33ba3ebcb03ac364a45d017139207af9
greghubdub <gregnagy10@gmail.com>
2017-05-09 19:15:30 -0500

    Save snippet for later string reverse function

--- js/explore.js
+++ js/explore.js
@@ -5,3 +5,21 @@
 // starting at 0
 // OR could loop thru forwards, put to new string starting at -1
 
+for (var i = 0; i > original.length; i++) {
+  flipped[-i] = original[i + 1];
+  console.log(flipped);
+}
+
+for (var i = 0; i < str.length; i++) {
+	console.log(str[i]);
+}
+
+for (var i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+
+for (var i = original.length; i > 0; i--) {
+  flipped[-i] = original[i + 1];
+  console.log(flipped);
+}
