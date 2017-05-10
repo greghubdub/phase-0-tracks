dcb310d0cb21535c850e2608b33c62b0c062eed3
greghubdub <gregnagy10@gmail.com>
2017-05-10 14:21:10 -0500

    Complete constructor function for car

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,30 @@ for (var i = 0; i < namesArr.length; i++) {
 }
 
 console.log(horseList);
+
+function Car(make, model, color) {
+	console.log("Building a new car")
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.horn = function() { console.log("Beep beep!"); }
+
+	console.log("Manufacture Complete")
+}
+
+var newCar = new Car("Ford", "Focus", "magenta");
+console.log(newCar);
+console.log("Watch out please!");
+newCar.horn();
+
+var anotherNewCar = new Car("Toyota", "Camry", "pink");
+console.log(anotherNewCar);
+console.log("No YOU watch out");
+anotherNewCar.horn();
+
+var yetAnotherCar = new Car("Chevy", "Malibu", "Tangerine");
+console.log(yetAnotherCar);
+console.log("Plenty of room on the road buddy");
+yetAnotherCar.horn();
+
+console.log(Car);
