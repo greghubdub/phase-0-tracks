17cc014492c8b241303d43406985d0a4e933296a
greghubdub <gregnagy10@gmail.com>
2017-05-10 13:23:17 -0500

    Create data structures and start driver code

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var colorsArr = ["turquiose", "aqua", "cerulean", "azure"];
+var namesArr = ["Musty", "Misty", "Stallone", "Bob"];
+
+console.log(colorsArr);
+console.log(namesArr);
+
+colorsArr.push("Indigo");
+namesArr.push("Valerie");
+
+console.log(colorsArr);
+console.log(namesArr);
+
