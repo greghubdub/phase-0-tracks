4eb89c55999678b22965642506322c5811885433
greghubdub <gregnagy10@gmail.com>
2017-05-09 19:01:37 -0500

    Update pseudocode for string reverse function

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,5 @@
 	// reverses it
 // loop thru string backwards? for each character starting with last, index it to a new string
 // starting at 0
+// OR could loop thru forwards, put to new string starting at -1
+
