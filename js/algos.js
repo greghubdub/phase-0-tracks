c8e8c7c897ac7acb47efdfa3e81b03f77ccb4893
greghubdub <gregnagy10@gmail.com>
2017-05-15 10:10:53 -0500

    Finish longest word function

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,16 @@
 // function that takes an array of words or phrases and returns the longest word or phrase
 // loop through array getting length of each element. Return largets amount
 
-var veggies = ['arugula', 'kale', 'cucumber', 'carrot']
+var veggies = ['arugula', 'kale', 'cucumber', 'carrot'];
+var buildings = ['Willis Tower', 'John Hancock building', 'City Hall']
+
+
+
+function LongWord(arr) {
+	var longestWord = arr.sort(function (a, b) { return b.length - a.length; })[0];
+	console.log(longestWord)
+}
+
+
+LongWord(veggies)
+LongWord(buildings)
