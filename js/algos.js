44957fd8b64db56a9ad53c87eaba39f8290fbf48
greghubdub <gregnagy10@gmail.com>
2017-05-15 09:49:20 -0500

    Create file for longest string array'

---
+++ js/algos.js
@@ -0,0 +1,4 @@
+// function that takes an array of words or phrases and returns the longest word or phrase
+// loop through array getting length of each element. Return largets amount
+
+var veggies = ['arugula', 'kale', 'cucumber', 'carrot']
