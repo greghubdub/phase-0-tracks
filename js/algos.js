150a294682ea417f44a8a0afe0a95fd68847c348
greghubdub <gregnagy10@gmail.com>
2017-05-15 11:13:32 -0500

    Created key-value pair matching function. Bugs are not worked out, but timeboxing and revisiting later

--- js/algos.js
+++ js/algos.js
@@ -1,16 +1,36 @@
+// LongWord
 // function that takes an array of words or phrases and returns the longest word or phrase
 // loop through array getting length of each element. Return largets amount
 
-var veggies = ['arugula', 'kale', 'cucumber', 'carrot'];
-var buildings = ['Willis Tower', 'John Hancock building', 'City Hall']
-
-
+// PairShare
+// check 2 objects for whether they share at least one key-value pair
+// loop through object 1 checking for match with EACH pair in object 2 (loop within loop?)
 
 function LongWord(arr) {
 	var longestWord = arr.sort(function (a, b) { return b.length - a.length; })[0];
 	console.log(longestWord)
 }
 
+var veggies = ['arugula', 'kale', 'cucumber', 'carrot'];
+var buildings = ['Willis Tower', 'John Hancock Building', 'City Hall']
 
 LongWord(veggies)
 LongWord(buildings)
+
+
+
+
+function PairShare(first, second) {
+	for (var key in first) {
+		if (second.hasOwnProperty(key)) && (first[key] == second[key]) {
+			return true
+		}
+	}
+}
+
+var dudes = {"Jeff":25, "Bob":29, "Rick": 32};
+var bros = {"Greg":30, "Jeff":25, "Paul": 27};
+
+PairShare(dudes, bros);
+
+
