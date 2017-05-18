13ce467fe6ed3f9b296989a4fe87a22a49d84329
greghubdub <gregnagy10@gmail.com>
2017-05-18 15:38:19 -0500

    Start kitten database over

---
+++ databases/kitten_maker/kitten_maker1.rb
@@ -0,0 +1,20 @@
+# OPERATION KITTEN EXPLOSION!
+
+# talk about Object Relational Mapping (ORM)
+
+# require gems
+
+# create SQLite3 database
+
+# learn about fancy string delimiters
+
+# create a kittens table (if it's not there already)
+
+# add a test kitten
+
+# explore ORM by retrieving data
+
+# add LOOOOTS of kittens!
+# so. many. kittens.
+# KittenExplosion
+
