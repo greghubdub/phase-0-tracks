aeb80b1a87df7a21c6a3dc527a208ae49c6ada04
greghubdub <gregnagy10@gmail.com>
2017-05-19 11:38:15 -0500

    Put user interface in the right spot ya dingus!

--- databases/food_inventory/food_tracker.rb
+++ databases/food_inventory/food_tracker.rb
@@ -38,22 +38,10 @@ db.execute(create_grocery_list_cmd)
 # and ask user if they want to remove
 
 # add test items
-db.execute("INSERT INTO inventory (product_general, product_specific, product_category, 
-	num_of_product, size_of_product) VALUES ('kipper snacks', 'King Oscar lightly smoked 
-	herring fillets', 'canned fish', 1, '3.25 oz')")
+# db.execute("INSERT INTO inventory (product_general, product_specific, product_category, 
+# 	num_of_product, size_of_product) VALUES ('kipper snacks', 'King Oscar lightly smoked 
+# 	herring fillets', 'canned fish', 1, '3.25 oz')")
 
-# USER INTERFACE
-# make it easy for user to add or remove items from either list
-
-puts "Do you want to add an item to the grocery list or to the existing food inventory?"
-
-which_list = gets.chomp
-
-if which_list == 'grocery' || which_list == 'grocery list'
-	add_grocery_list_item
-elsif which_list == 'inventory' || which_list == 'food inventory' || which_list == 'existing'
-	add_inventory_item
-end
 
 def add_grocery_list_item
 	puts "Enter the general item name"
@@ -89,3 +77,16 @@ def add_inventory_item
 	db.execute("SELECT * FROM inventory")
 end
 
+
+# USER INTERFACE
+# make it easy for user to add or remove items from either list
+
+puts "Do you want to add an item to the grocery list or to the existing food inventory?"
+
+which_list = gets.chomp
+
+if which_list == 'grocery' || which_list == 'grocery list'
+	add_grocery_list_item
+elsif which_list == 'inventory' || which_list == 'food inventory' || which_list == 'existing'
+	add_inventory_item
+end
