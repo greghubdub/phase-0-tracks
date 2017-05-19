58cfa329608deda8109508ec09f7edcefa6d834a
greghubdub <gregnagy10@gmail.com>
2017-05-19 10:34:38 -0500

    Start psedocode for inventory project

---
+++ databases/food_inventory/food_tracker.rb
@@ -0,0 +1,13 @@
+# FOOD INVENTORY AND GROCERY LIST DATABASE
+
+# require gems
+require 'sqlite3'
+
+# table for food inventory
+
+# table for grocery list
+
+# add test items
+
+# make it easy for user to add or remove items from either list
+
