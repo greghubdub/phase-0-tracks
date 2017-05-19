45eea44c4c513419bfd7f4145ab9cb3f716e2946
greghubdub <gregnagy10@gmail.com>
2017-05-19 10:42:27 -0500

    Continue pseudocode and planning for inventory

--- databases/food_inventory/food_tracker.rb
+++ databases/food_inventory/food_tracker.rb
@@ -3,9 +3,25 @@
 # require gems
 require 'sqlite3'
 
+# database has tables for current inventory and grocery items to be picked up
+db = SQLite3::Database.new("vittles.db")
+
 # table for food inventory
+create_inventory_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS inventory(
+		id INTEGER PRIMARY KEY,
+		product_general VARCHAR(255),
+		product_specific VARCHAR(255),
+		num_of_product INT,
+		size_of_product INT,
+
+	)
+SQL
 
 # table for grocery list
+
+# when a certain product_general is added to inventory, check grocery_list for same
+# and ask user if they want to remove
 
 # add test items
 
